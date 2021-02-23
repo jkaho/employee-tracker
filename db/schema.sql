89424d5f05ec3fb6c8e7044cafce2a524a53bfba
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-23 14:44:10 +1100

    Modify 'schema.sql' to add employee table

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,9 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30)
+);
