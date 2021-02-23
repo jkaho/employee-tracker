aa864a21df7077fd77446145b653e28b4fc2d98e
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-23 14:54:25 +1100

    Modify 'schema.sql' to edit primar y key syntax

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30),
-    last_name VARCHAR(30)
+    last_name VARCHAR(30),
+    PRIMARY KEY(id),
     FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE SET NULL,
     FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30),
     salary DECIMAL,
+    PRIMARY KEY(id),
     FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL,
+    PRIMARY KEY(id),
     name VARCHAR(30)
 );
