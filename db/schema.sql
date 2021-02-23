761735eeb761b1fd4a45135c132c257cb58d477d
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-23 14:50:55 +1100

    Modify 'schema.sql' to add foreign keys to employee table

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,8 @@ CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30)
+    FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
 CREATE TABLE role (
