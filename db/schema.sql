8e01be1ef168d51b76f76e73a30099b5778c16ce
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-23 15:42:22 +1100

    Modify 'schema.sql' to edit syntax

--- db/schema.sql
+++ db/schema.sql
@@ -3,25 +3,34 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employee (
+CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    PRIMARY KEY(id),
-    FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE SET NULL,
-    FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    name VARCHAR(30),
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30),
     salary DECIMAL,
+    department_id INT,
     PRIMARY KEY(id),
-    FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE SET NULL
+    FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
-CREATE TABLE department (
+CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(30),
-    PRIMARY KEY(id)
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
 );
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
+
