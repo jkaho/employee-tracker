60ddc7ebec66ae95ace557b36ae3dc13f590429e
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-23 16:23:35 +1100

    Modify 'index.js' to create MySQL connection

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    // Your port
+    port: 3306,
+    // Your username
+    user: 'root',
+    // Your MySQL password
+    password: 'pw',
+    database: 'employee_db'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+});
